96c94e1977755099ab06e504e7876dd111b8b00a
Ali Abbas <116796416+Aliabbas9870@users.noreply.github.com>
2024-05-04 14:05:33 +0400

    MY portfolio Ali Abbas

---
+++ components/belogSection.php
@@ -0,0 +1,78 @@
+	<!-- Blog section -->
+    <div class="section-box mt-4" id="blog">
+							<div class="row">
+								<div class="col-12 col-md-10 col-xl-8">
+									<h6 class="title-heading mb-3" data-backdrop-text="Blog">Blog</h6>
+									<h1>Latest Blog Posts</h1>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+									tempor incididunt ut labore</p>
+								</div>
+							</div>
+							<div class="mt-4 mt-lg-5">
+								<!-- Blog Post box 1 -->
+								<div class="blog-post-box">
+									<div class="blog-post-img">
+										<a href="#">
+											<img src="assets/images/blog-post-1.jpg" alt="">
+										</a>
+										<div class="blog-post-category">
+											<a href="#">Experience</a>
+										</div>
+									</div>
+									<div class="blog-post-caption">
+										<p class="mb-2">Posted on Aug 21</p>
+										<h2><a class="link-decoration" href="blog-single.html">4 years of working from home</a></h2>
+										<a class="button button-sm button-outline mt-2" href="blog-single.html">Read more</a>
+									</div>
+								</div>
+								<!-- Blog Post box 2 -->
+								<div class="blog-post-box">
+									<div class="blog-post-img">
+										<a href="#">
+											<img src="assets/images/blog-post-2.jpg" alt="">
+										</a>
+										<div class="blog-post-category">
+											<a href="#">Health</a>
+										</div>
+									</div>
+									<div class="blog-post-caption">
+										<p class="mb-2">Posted on Aug 27</p>
+										<h2><a class="link-decoration" href="blog-single.html">Importance of outdoor times</a></h2>
+										<a class="button button-sm button-outline mt-2" href="blog-single.html">Read more</a>
+									</div>
+								</div>
+								<!-- Blog Post box 3 -->
+								<div class="blog-post-box">
+									<div class="blog-post-img">
+										<a href="#">
+											<img src="assets/images/blog-post-3.jpg" alt="">
+										</a>
+										<div class="blog-post-category">
+											<a href="#">Foods & Drinks</a>
+										</div>
+									</div>
+									<div class="blog-post-caption">
+										<p class="mb-2">Posted on Aug 26</p>
+										<h2><a class="link-decoration" href="blog-single.html">Fun drinks to make at home</a></h2>
+										<a class="button button-sm button-outline mt-2" href="blog-single.html">Read more</a>
+									</div>
+								</div>
+								<!-- Blog Post box 4 -->
+								<div class="blog-post-box">
+									<div class="blog-post-img">
+										<a href="#">
+											<img src="assets/images/blog-post-4.jpg" alt="">
+										</a>
+										<div class="blog-post-category">
+											<a href="#">Health</a>
+										</div>
+									</div>
+									<div class="blog-post-caption">
+										<p class="mb-2">Posted on Aug 25</p>
+										<h2><a class="link-decoration" href="blog-single.html">Benefits of eating healthy</a></h2>
+										<a class="button button-sm button-outline mt-2" href="blog-single.html">Read more</a>
+									</div>
+								</div>
+							</div>
+						</div><!-- end section-box -->
+						<!-- end Blog section -->
